7a66281323ec99a881f3a6ea191c3d03b9731f19
Oein <oein0219@gmail.com>
2024-02-03 19:16:08 +0900

    add dsu support

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,6 +4,7 @@ import net from "net";
 import path from "path";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { ChildProcess, execFile } from "child_process";
+import { DSUServer } from "node-dsu";
 
 const pkg = (process as any).pkg;
 const basePath = pkg ? path.dirname(process.execPath) : __dirname;
@@ -12,6 +13,7 @@ const vigemPath = path.join(basePath, "vigem.exe");
 
 let client: net.Socket;
 let child: ChildProcess;
+let dsu: DSUServer;
 
 const anyKey2Exit = () => {
   console.log("Press any key to exit");
@@ -24,10 +26,25 @@ const anyKey2Exit = () => {
   });
 };
 
-const main = () => {
+const ensureVigem = async () => {
+  if (existsSync(vigemPath)) return;
+  console.log("Downloading vigem.exe");
+  const file = createWriteStream(vigemPath);
+  const URL =
+    "https://github.com/henrikvik/vigem-interface/releases/download/1.0/vigem-interface.exe";
+  return new Promise<void>((resolve, reject) => {
+    downloadFile(URL, vigemPath).then(() => {
+      console.log("Downloaded vigem.exe");
+      anyKey2Exit();
+    });
+  });
+};
+
+const main = async () => {
   client = new net.Socket();
 
   const config = JSON.parse(readFileSync(configPath).toString());
+  if (!(config.DSU_MODE === true)) await ensureVigem();
 
   const parseData = (data: Buffer) => {
     // console.log(data);
@@ -66,69 +83,127 @@ const main = () => {
       LDOWN: b(2, 5),
     };
 
-    const maxv = 1;
-    const minv = -1;
+    if (config.DSU_MODE === true && dsu) {
+      dsu.DSU_setControllerState(0, {
+        connected: true,
+        DPAD: {
+          UP: Parsed.DUP,
+          DOWN: Parsed.DDOWN,
+          LEFT: Parsed.DLEFT,
+          RIGHT: Parsed.DRIGHT,
+        },
+        LSTICK: {
+          X: Parsed.LLEFT ? 0 : Parsed.LRIGHT ? 255 : 128,
+          Y: Parsed.LUP ? 0 : Parsed.LDOWN ? 255 : 128,
+        },
+        RSTICK: {
+          X: Parsed.CLEFT ? 0 : Parsed.CRIGHT ? 255 : 128,
+          Y: Parsed.CUP ? 0 : Parsed.CDOWN ? 255 : 128,
+        },
+        ANALOG: {
+          DPAD: {
+            LEFT: Parsed.DLEFT ? 255 : 0,
+            RIGHT: Parsed.DRIGHT ? 255 : 0,
+            UP: Parsed.DUP ? 255 : 0,
+            DOWN: Parsed.DDOWN ? 255 : 0,
+          },
+          KEYS: {
+            A: Parsed.A ? 255 : 0,
+            B: Parsed.B ? 255 : 0,
+            X: Parsed.X ? 255 : 0,
+            Y: Parsed.Y ? 255 : 0,
+            L1: Parsed.L ? 255 : 0,
+            R1: Parsed.R ? 255 : 0,
+            L2: Parsed.ZL ? 255 : 0,
+            R2: Parsed.ZR ? 255 : 0,
+          },
+        },
+        MOTION: {
+          TIMESTAMP: Date.now(),
+          ACCEL: {
+            X: 0,
+            Y: 0,
+            Z: 0,
+          },
+          GYRO: {
+            PITCH: 0,
+            ROLL: 0,
+            YAW: 0,
+          },
+        },
+        KEYS: {
+          HOME: false,
+          OPTIONS: Parsed.START,
+          SHARE: Parsed.SELECT,
+          L3: false,
+          R3: false,
+        },
+      });
+    } else {
+      const maxv = 1;
+      const minv = -1;
 
-    const dt = (v: boolean, data: any) => data + (v ? "+" : "-");
+      const dt = (v: boolean, data: any) => data + (v ? "+" : "-");
 
-    const getNotiation = (
-      up: boolean,
-      down: boolean,
-      left: boolean,
-      right: boolean
-    ) => {
-      if (up && !down && !left && !right) return "8";
-      if (up && !down && !left && right) return "9";
-      if (up && !down && left && !right) return "7";
-      if (!up && !down && left && !right) return "4";
-      if (!up && !down && !left && right) return "6";
-      if (!up && down && !left && !right) return "2";
-      if (!up && down && !left && right) return "3";
-      if (!up && down && left && !right) return "1";
-      return "5";
-    };
+      const getNotiation = (
+        up: boolean,
+        down: boolean,
+        left: boolean,
+        right: boolean
+      ) => {
+        if (up && !down && !left && !right) return "8";
+        if (up && !down && !left && right) return "9";
+        if (up && !down && left && !right) return "7";
+        if (!up && !down && left && !right) return "4";
+        if (!up && !down && !left && right) return "6";
+        if (!up && down && !left && !right) return "2";
+        if (!up && down && !left && right) return "3";
+        if (!up && down && left && !right) return "1";
+        return "5";
+      };
 
-    const mf = (dt: string, pre: string) => pre + dt;
+      const mf = (dt: string, pre: string) => pre + dt;
 
-    const getDPad = () =>
-      mf(
-        getNotiation(Parsed.DUP, Parsed.DDOWN, Parsed.DLEFT, Parsed.DRIGHT),
-        "D"
-      );
+      const getDPad = () =>
+        mf(
+          getNotiation(Parsed.DUP, Parsed.DDOWN, Parsed.DLEFT, Parsed.DRIGHT),
+          "D"
+        );
 
-    // console.log("AN", Parsed.ANALOGLSTICK);
-    const getLStick = () =>
-      mf(
-        getNotiation(Parsed.LUP, Parsed.LDOWN, Parsed.LLEFT, Parsed.LRIGHT),
-        "L"
-      );
+      // console.log("AN", Parsed.ANALOGLSTICK);
+      const getLStick = () =>
+        mf(
+          getNotiation(Parsed.LUP, Parsed.LDOWN, Parsed.LLEFT, Parsed.LRIGHT),
+          "L"
+        );
 
-    const getRStick = () =>
-      mf(
-        getNotiation(Parsed.CUP, Parsed.CDOWN, Parsed.CLEFT, Parsed.CRIGHT),
-        "R"
-      );
+      const getRStick = () =>
+        mf(
+          getNotiation(Parsed.CUP, Parsed.CDOWN, Parsed.CLEFT, Parsed.CRIGHT),
+          "R"
+        );
 
-    const command = `${[
-      dt(Parsed.A, "A"),
-      dt(Parsed.B, "B"),
-      dt(Parsed.X, "X"),
-      dt(Parsed.Y, "Y"),
-      dt(Parsed.L, "LB"),
-      dt(Parsed.R, "RB"),
-      dt(Parsed.ZL, "L"),
-      dt(Parsed.ZR, "R"),
-      dt(Parsed.SELECT, "BACK"),
-      dt(Parsed.START, "START"),
-      getDPad(),
-      getLStick(),
-      getRStick(),
-    ]
-      .filter((x) => x != "")
-      .join(" ")}`;
+      const command = `${[
+        dt(Parsed.A, "A"),
+        dt(Parsed.B, "B"),
+        dt(Parsed.X, "X"),
+        dt(Parsed.Y, "Y"),
+        dt(Parsed.L, "LB"),
+        dt(Parsed.R, "RB"),
+        dt(Parsed.ZL, "L"),
+        dt(Parsed.ZR, "R"),
+        dt(Parsed.SELECT, "BACK"),
+        dt(Parsed.START, "START"),
+        getDPad(),
+        getLStick(),
+        getRStick(),
+      ]
+        .filter((x) => x != "")
+        .join(" ")}`;
 
-    console.log("> " + command);
-    child.stdin?.write(command + "\n");
+      console.log("> " + command);
+      child.stdin?.write(command + "\n");
+    }
   };
 
   client.on("data", (data) => {
@@ -152,11 +227,23 @@ const main = () => {
   console.log("CONNECTING TO 3DS");
   client.connect(config["3DS_PORT"], config["3DS_IP"], () => {
     console.log("CONNECTED TO 3DS");
-    child = execFile(vigemPath);
-    child.on("exit", (code, signal) => {
-      console.log("Vigem exited with code", code, signal);
-      anyKey2Exit();
-    });
+    if (!(config.DSU_MODE === true)) {
+      child = execFile(vigemPath);
+      child.on("exit", (code, signal) => {
+        console.log("Vigem exited with code", code, signal);
+        anyKey2Exit();
+      });
+    } else {
+      dsu = new DSUServer({
+        autoStart: true,
+        port: 25650,
+      });
+      dsu.DSU_setControllerState(0, {
+        ...dsu.DSU_dummyState(),
+        connected: true,
+      });
+      console.log("DSU Server started on port 25650");
+    }
 
     // anyKey2Exit();
   });
@@ -198,19 +285,6 @@ async function downloadFile(fileUrl: string, outputLocationPath: string) {
     });
   });
 }
-const ensureVigem = async () => {
-  if (existsSync(vigemPath)) return;
-  console.log("Downloading vigem.exe");
-  const file = createWriteStream(vigemPath);
-  const URL =
-    "https://github.com/henrikvik/vigem-interface/releases/download/1.0/vigem-interface.exe";
-  return new Promise<void>((resolve, reject) => {
-    downloadFile(URL, vigemPath).then(() => {
-      console.log("Downloaded vigem.exe");
-      anyKey2Exit();
-    });
-  });
-};
 
 if (!existsSync(configPath)) {
   writeFileSync(
@@ -219,6 +293,7 @@ if (!existsSync(configPath)) {
       {
         "3DS_IP": "127.0.0.1",
         "3DS_PORT": 80,
+        DSU_MODE: false,
       },
       null,
       2
@@ -226,4 +301,4 @@ if (!existsSync(configPath)) {
   );
   console.error("Please configure the config.json file");
   anyKey2Exit();
-} else ensureVigem().then(main).catch(console.error);
+} else main().catch(console.error);
